839921d5d72069883e04ab6d79a37d1c864a9aa0
Jeffrey A Mills <millsjf@uccob.uc.edu>
2018-04-28 08:25:56 -0400

    added mcodds, bayespval

--- src/BayesTesting.jl
+++ src/BayesTesting.jl
@@ -1,7 +1,8 @@
 module BayesTesting
 
-export standardize_scores, update_mean, marginal_posterior_mu
+export standardize_scores, update_mean, marginal_posterior_mu, mcodds, todds, bayespval
 # package code goes here
 include("bayes_meta_analysis.jl")
+include("bayes_odds_pvalue.jl")
 
 end # module
