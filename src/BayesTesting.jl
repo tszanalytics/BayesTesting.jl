587244e342237ef25a58401e6b0d01e97ceeb545
Jeff Mills <tszanalytics@gmail.com>
2018-06-28 16:19:06 -0400

    add compare functions

--- src/BayesTesting.jl
+++ src/BayesTesting.jl
@@ -1,6 +1,6 @@
 module BayesTesting
 
-export update_mean, marginal_posterior_mu, mcodds, todds, bayespval, blinreg, bayesregNIG, bayesreg, gsreg
+export update_mean, marginal_posterior_mu, mcodds, todds, bayespval, blinreg, bayesregNIG, bayesreg, gsreg, compare_means, compare_proportions,beta_posterior,beta_update,equiv_test
 # package code goes here
 include("bayes_meta_analysis.jl")
 include("bayes_odds_pvalue.jl")
