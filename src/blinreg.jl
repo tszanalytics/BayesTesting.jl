b43865188b54b828c4d280f482a6b0203e0d9ee2
Jeff Mills <tszanalytics@gmail.com>
2019-09-18 14:52:05 -0400

    Added AIC, BIC and p-values

--- src/blinreg.jl
+++ src/blinreg.jl
@@ -15,29 +15,39 @@ function blinreg(y,x)
   X = [ones(n) x]
   b = (X'*X) \ X'*y
   resids = y - X*b
+  RSS = sum(resids.^2)
+  s2 = RSS/n
 
-  s2 = sum(resids.^2)/n
   covb = s2.*inv(X'X)
   seb = sqrt.(diag(covb))
   k = length(b)
+  ### Correct formulas for AIC and BIC
+  BIC = n*log(s2) + k*log(n)
+  AIC = 2*k + n*log(s2)
   odds = zeros(k)
+  pvals = zeros(k)
+  that = abs.(b)./seb
   for i in 1:k
-    odds[i] = todds(b[i],seb[i],(n-k))
+    odds[i] = (1.0 + (that[i]^2)/(n-k))^((n-k+1)/2.0)
+    pvals[i] = 1.0 - cdf(TDist(n-k), that[i])
   end
-  println("coeffs = ",round.(b,3))
-  println(" s.e's = ",round.(seb,3))
-  println(" odds = ",round.(odds,3))
-  println("s^2 (eqn. variance) = ",round(s2,6))
+  println("coeffs = ", round.(b, digits=3))
+  println(" s.e's = ", round.(seb, digits=3))
+  println(" odds = ", round.(odds, digits=3))
+  println("p-values = ", round.(pvals, digits=4))
+  println("s^2 (eqn. variance) = ",round(s2, digits=6))
 
   # compute R^2
   tss = sum((y .- mean(y)).^2)
-  R2 = 1.0 - s2*n/tss
-  println("Rsquared = ",round(R2,3))
+
+  R2 = 1 - s2*n/tss
+  println("Rsquared = ",round(R2, digits=3))
+  println("AIC = ", round(AIC, digits = 2), "  BIC = ", round(BIC, digits = 2),)
   s = sqrt(s2)
-  return b, seb, odds, s, R2
+  return b, seb, odds, pvals, s, R2, RSS, AIC, BIC
 end
 
 ## example of use:
 #x = randn(20)
 #y = 1 + 1.*x .+ randn(20)
-#b, seb, odds, s,R2 = blinreg(y,x)
+#b, seb, odds, pvals, s, R2, RSS, AIC, BIC = blinreg(y,x)
